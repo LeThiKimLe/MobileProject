313ba2167fccd85306e815129b1a2e32c23db480
datdnt1027 <103656154+datdnt1027@users.noreply.github.com>
2023-05-08 13:56:45 +0700

    Config file

--- src/main/java/dao/SQLServerConnection.java
+++ src/main/java/dao/SQLServerConnection.java
@@ -6,28 +6,22 @@ import java.sql.SQLException;;
 
 public class SQLServerConnection {
 	
-	private static final String dbDriver ="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static final String serverName = "LAPTOP-N0HFUMP2";
-	private static final String dbName = "OnlineLearningSystem";
-	private static final String portNumber = "1433";
-	private static final String instance = "";// MSSQLSERVER LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
-	private static final String userID = "sa";
-	private static final String password = "123456";
-
-	public static Connection initializeDatabase() throws SQLException, ClassNotFoundException {
+	public static Connection initializeDatabase()throws SQLException,ClassNotFoundException
+	{
 		
-		String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName 
-				+ ";encrypt=true;trustServerCertificate=true;";
-		if (instance == null || instance.trim().isEmpty())
-			url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName 
-			+ ";encrypt=true;trustServerCertificate=true;";
-		
-		Connection conn = null;
+		String dbDriver ="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		String dbURL ="jdbc:sqlserver://localhost:1433";
+		String dbName = "OnlineLearningSystem";
+		String dbUsername = "sa";
+		String dbPassword = "123456";
+		String connectionURL = dbURL + ";databaseName="+dbName + ";encrypt=true;trustServerCertificate=true;";
+		Connection conn=null;
 		try {
 			Class.forName(dbDriver);
-			conn = DriverManager.getConnection(url, userID, password);
-		} catch (Exception ex) {
-			// TODO: handle exception
+			conn=DriverManager.getConnection(connectionURL, dbUsername, dbPassword);
+			System.out.print("Connection successfully");
+		} catch (Exception ex){
+			System.out.print("Connection failure");
 			ex.printStackTrace();
 		}
 		return conn;
