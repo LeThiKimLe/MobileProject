30d73f3568c059f8c75acd0cca697ca0ed289fe8
datdnt1027 <103656154+datdnt1027@users.noreply.github.com>
2023-05-17 01:44:33 +0700

    Update Login API

--- src/main/java/servlet/DangNhapControl/API/DangNhapAPIController.java
+++ src/main/java/servlet/DangNhapControl/API/DangNhapAPIController.java
@@ -6,7 +6,9 @@ import java.sql.SQLException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import bean.DangNhap;
+import bean.API.GiaoVienAPI;
 import bean.API.HocVienAPI;
+import bean.API.QuanTriVienAPI;
 import dao.ConnectDataBase;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -26,7 +28,7 @@ public class DangNhapAPIController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		response.setContentType("application/json;charset=UTF-8");
 		Connection conn = null;
 		try {
@@ -43,26 +45,61 @@ public class DangNhapAPIController extends HttpServlet {
 		String password = new String(request.getParameter("password").getBytes("ISO-8859-1"), "UTF-8");
 
 		DangNhap accountDangNhap = null;
-		HocVienAPI hvApi = new HocVienAPI();
+		QuanTriVienAPI qtv = new QuanTriVienAPI();
+		GiaoVienAPI gv = new GiaoVienAPI();
+		HocVienAPI hv = new HocVienAPI();
 
 		try {
 			accountDangNhap = DBUtils.YeuCauDangNhap(conn, username, password);
 			if (accountDangNhap != null && conn != null) {
-				hvApi.setResult("success");
-				hvApi.setHocVien(DBUtils.LayThongTin(conn, accountDangNhap.getIdString()));
-			} else {
-				hvApi.setResult("fail");
-				if(!DBUtils.CheckUsername(conn, username)) {
-					hvApi.setMessage("Mật khẩu sai");
+				System.out.println(accountDangNhap.getRole());
+				if (accountDangNhap.getRole().contains("QTV")) {
+					qtv.setResult("success");
+					qtv.setQtv(DBUtils.LayThongTinQTV(conn, accountDangNhap.getIdString()));
+				} else if (accountDangNhap.getRole().contains("GV")) {
+					gv.setResult("success");
+					gv.setGiaoVien(DBUtils.LayThongTinGiaoVien(conn, accountDangNhap.getIdString()));
+				} else if (accountDangNhap.getRole().contains("HV")) {
+					hv.setResult("success");
+					hv.setHocVien(DBUtils.LayThongTin(conn, accountDangNhap.getIdString()));
 				}
-				else {
-					hvApi.setMessage("Tài khoản và mật khẩu không tồn tại");
+
+			}else {
+				String message = "Sai mật khẩu";
+				if(DBUtils.CheckUsernameQTV(conn, username)) {
+					qtv.setResult("fail");
+					qtv.setMessage(message);
+				}else if(DBUtils.CheckUsernameGiaoVien(conn, username)) {
+					gv.setResult("fail");
+					gv.setMessage(message);
+				}else {
+					hv.setResult("fail");
+					hv.setMessage(message);
 				}
 			}
 			ObjectMapper Obj = new ObjectMapper();
-			conn.close();
-			Obj.writeValue(response.getOutputStream(), hvApi);
 
+			if (accountDangNhap != null ) {
+				if (accountDangNhap.getRole().contains("QTV")) {
+					Obj.writeValue(response.getOutputStream(), qtv);
+				} else if (accountDangNhap.getRole().contains("GV")) {
+					Obj.writeValue(response.getOutputStream(), gv);
+				} else {
+					Obj.writeValue(response.getOutputStream(), hv);
+				}
+			}else if (DBUtils.CheckUsername(conn, username)) {
+				if (DBUtils.CheckUsernameQTV(conn, username)) {
+					Obj.writeValue(response.getOutputStream(), qtv);
+				} else if (DBUtils.CheckUsernameGiaoVien(conn, username)) {
+					Obj.writeValue(response.getOutputStream(), gv);
+				} else {
+					Obj.writeValue(response.getOutputStream(), hv);
+				}
+			} 
+			else{
+				Obj.writeValue(response.getOutputStream(), null);
+			}
+			conn.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
